889b7996cd6f39924b217e8ea1c82ef2f2a3aad6
Oliver <gelutou@163.com>
2021-02-19 16:37:22 +0800

    完成poi

---
+++ poi/src/test/java/oliver/poi/easyexcel/BaseRead.java
@@ -0,0 +1,32 @@
+package oliver.poi.easyexcel;
+
+import com.alibaba.excel.EasyExcel;
+import oliver.poi.DemoData;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Oliver
+ * @date 2021/2/19 16:18
+ * @description
+ */
+public class BaseRead {
+
+    String filePath = "D:\\utility\\poi\\";
+
+    /**
+     * 最简单的读
+     * <p>1. 创建excel对应的实体对象 参照{@link DemoData}
+     * <p>2. 由于默认一行行的读取excel，所以需要创建excel一行一行的回调监听器，参照{@link DemoDataListener}
+     * <p>3. 直接读即可
+     */
+    @Test
+    public void simpleRead() {
+        // 有个很重要的点 DemoDataListener 不能被spring管理，要每次读取excel都要new,然后里面用到spring可以构造方法传进去
+        // 写法1：
+        String fileName = filePath + "easyTest.xlsx";
+        // 这里 需要指定读用哪个class去读，然后读取第一个sheet 文件流会自动关闭
+        //EasyExcel.read(fileName, DemoData.class, new DemoDataListener()).sheet().doRead();
+        //读取所有sheet
+        EasyExcel.read(fileName, DemoData.class, new DemoDataListener()).doReadAll();
+    }
+}
--- poi/src/test/java/oliver/poi/easyexcel/BaseWrite.java
+++ poi/src/test/java/oliver/poi/easyexcel/BaseWrite.java
@@ -1,6 +1,9 @@
 package oliver.poi.easyexcel;
 
 
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.write.metadata.WriteSheet;
 import oliver.poi.DemoData;
 import org.junit.jupiter.api.Test;
 
@@ -15,6 +18,8 @@ import java.util.List;
  */
 public class BaseWrite {
 
+    String filePath = "D:\\utility\\poi\\";
+
     @Test
     public void data() {
         List<DemoData> list = new ArrayList<>();
@@ -26,5 +31,26 @@ public class BaseWrite {
             list.add(data);
         }
         System.out.println("list.size() = " + list.size());
+
+        // 写法1
+        String fileName = filePath+"easyTest.xlsx";
+        // 方法2 如果写到不同的sheet 同一个对象
+        ExcelWriter excelWriter = null;
+        try {
+            // 这里 指定文件
+            excelWriter = EasyExcel.write(fileName, DemoData.class).build();
+            // 去调用写入,这里我调用了五次，实际使用时根据数据库分页的总的页数来。这里最终会写到5个sheet里面
+            for (int i = 0; i < 5; i++) {
+                // 每次都要创建writeSheet 这里注意必须指定sheetNo 而且sheetName必须不一样
+                WriteSheet writeSheet = EasyExcel.writerSheet(i, "模板" + i).build();
+                // 分页去数据库查询数据 这里可以去数据库查询每一页的数据
+                excelWriter.write(list, writeSheet);
+            }
+        } finally {
+            // 千万别忘记finish 会帮忙关闭流
+            if (excelWriter != null) {
+                excelWriter.finish();
+            }
+        }
     }
 }
